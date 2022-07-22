ea4dce80fa44dbf90f7f72a5b03b56489858f3f2
Victor <victorruand.dev@gmail.com>
2022-07-22 09:09:25 -0300

    FEAT: tab de sair;

---
+++ lib/src/features/home/view/components/tab_bar_views/go_out_tab.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class GoOutTab extends StatelessWidget {
+  const GoOutTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Sair',
+      ),
+    );
+  }
+}
