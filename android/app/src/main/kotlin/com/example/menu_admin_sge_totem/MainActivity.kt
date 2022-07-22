16f96794c184a0b5691f0bfed50a1dc8dedf423a
Victor <victorruand.dev@gmail.com>
2022-07-22 09:11:37 -0300

    CHORE: Adicionando arquivos gerais;

---
+++ android/app/src/main/kotlin/com/example/menu_admin_sge_totem/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.menu_admin_sge_totem
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/app_widget.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:menu_admin_sge_totem/src/features/home/view/home_page.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
