66adc94f673180d7752238d8341c1afe78f1b514
Victor <victorruand.dev@gmail.com>
2022-07-25 09:12:56 -0300

    CHORE: add alert de cancelamento;

--- lib/src/features/home/view/components/tab_bar_views/cancelation_tab.dart
+++ lib/src/features/home/view/components/tab_bar_views/cancelation_tab.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:menu_admin_sge_totem/src/features/home/view/components/custom_line_dash.dart';
+import 'package:menu_admin_sge_totem/src/features/home/view/components/custom_line_dash_widget.dart';
+import 'package:menu_admin_sge_totem/src/features/home/view/components/footer_button_widget.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 class CancelationTab extends StatelessWidget {
   const CancelationTab({Key? key}) : super(key: key);
@@ -15,7 +17,6 @@ class CancelationTab extends StatelessWidget {
       fontSize: 20,
       fontWeight: FontWeight.w500,
     );
-    Size size = MediaQuery.of(context).size;
     return Column(
       children: [
         Padding(
@@ -67,15 +68,12 @@ class CancelationTab extends StatelessWidget {
             ),
           ],
         ),
-        // const Divider(
-        //   height: 18,
-        // ),
-        const CustomLineDash(),
+        const CustomLineDashWidget(),
         ListView.separated(
           shrinkWrap: true,
           itemCount: 2,
           separatorBuilder: (BuildContext context, int index) =>
-              const CustomLineDash(),
+              const CustomLineDashWidget(),
           itemBuilder: (BuildContext context, int index) {
             return Padding(
               padding: const EdgeInsets.symmetric(vertical: 23),
@@ -111,8 +109,9 @@ class CancelationTab extends StatelessWidget {
                   Padding(
                     padding: const EdgeInsets.only(right: 30),
                     child: InkWell(
-                        onTap: () {},
-                        child: const FaIcon(FontAwesomeIcons.trashCan)),
+                      onTap: () => _onAlertItemCancellation(context),
+                      child: const FaIcon(FontAwesomeIcons.trashCan),
+                    ),
                   ),
                 ],
               ),
@@ -121,49 +120,81 @@ class CancelationTab extends StatelessWidget {
         ),
         const Expanded(
           child: SizedBox(),
-          // flex: 4,
         ),
-        Padding(
-          padding: const EdgeInsets.only(bottom: 70, left: 77),
-          child: Row(
-            children: [
-              InkWell(
-                onTap: () {},
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(30),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.1),
-                        offset: const Offset(
-                          8.0,
-                          7.0,
-                        ),
-                        blurRadius: 4.0,
-                        spreadRadius: 1.0,
-                      ), //BoxShadow
-                    ],
-                  ),
-                  child: const Icon(
-                    Icons.arrow_back,
-                    size: 55,
-                    color: Colors.blue,
-                  ),
-                ),
-              ),
-              SizedBox(width: size.width * 0.34765625),
-              InkWell(
-                onTap: () {},
-                child: const Text(
-                  'Cancelar Compra',
-                  style: TextStyle(color: Colors.blue, fontSize: 25),
-                ),
-              )
-            ],
-          ),
-        )
+        FooterButtonWidget(
+          buttonText: 'Cancelamento',
+          onTap: () {},
+        ),
       ],
     );
   }
+}
+
+_onAlertItemCancellation(context) {
+  final Size size = MediaQuery.of(context).size;
+  Alert(
+    title: 'Cancelamento de item',
+    onWillPopActive: true,
+    style: AlertStyle(
+      isCloseButton: false,
+      titleStyle: Theme.of(context)
+          .textTheme
+          .headline4!
+          .copyWith(fontSize: size.width * 0.025, color: Colors.black),
+    ),
+    context: context,
+    content: SizedBox(
+      width: 630,
+      child: Column(
+        children: [
+          SizedBox(height: size.height * 0.06),
+          const Text(
+            'Deseja confirmar cancelamento do item:',
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.w400),
+          ),
+          SizedBox(height: size.height * 0.05),
+          const Text(
+            'Arroz branco Camil Tipo 1 5Kg',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: size.height * 0.06),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: size.width * 0.0065),
+                child: InkWell(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text(
+                    'Cancelar',
+                    style: Theme.of(context).textTheme.button!.copyWith(
+                          color: const Color.fromRGBO(55, 80, 255, 1),
+                          fontSize: MediaQuery.of(context).size.width * 0.016,
+                        ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: size.width * 0.0065),
+                child: InkWell(
+                  onTap: () {}, // diminui uma quantidade
+                  child: Text(
+                    'Confirmar',
+                    style: Theme.of(context).textTheme.button!.copyWith(
+                        color: const Color.fromRGBO(55, 80, 255, 1),
+                        fontSize: MediaQuery.of(context).size.width * 0.016),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    ),
+    buttons: [],
+  ).show();
 }
