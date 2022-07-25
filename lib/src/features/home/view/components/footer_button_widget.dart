811dc89560cbfd94a9b43d57e3336da8bae29142
Victor <victorruand.dev@gmail.com>
2022-07-25 08:34:36 -0300

    FEAT: Footer com dois buttons para utilização nas páginas do app;

---
+++ lib/src/features/home/view/components/footer_button_widget.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class FooterButtonWidget extends StatelessWidget {
+  final String buttonText;
+  final Function()? onTap;
+
+  const FooterButtonWidget({Key? key, required this.buttonText, this.onTap})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 70, left: 77),
+      child: Row(
+        children: [
+          InkWell(
+            onTap: () {}, // voltar para o caixa
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    offset: const Offset(
+                      8.0,
+                      7.0,
+                    ),
+                    blurRadius: 4.0,
+                    spreadRadius: 1.0,
+                  ), //BoxShadow
+                ],
+              ),
+              child: const Icon(
+                Icons.arrow_back,
+                size: 55,
+                color: Colors.blue,
+              ),
+            ),
+          ),
+          SizedBox(width: size.width * 0.3156),
+          InkWell(
+            onTap: onTap,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.15),
+                    offset: const Offset(
+                      0.5,
+                      3.3,
+                    ),
+                    blurRadius: 4.0,
+                    spreadRadius: 1.0,
+                    blurStyle: BlurStyle.inner,
+                  ), //BoxShadow
+                ],
+              ),
+              child: SizedBox(
+                width: 300,
+                height: 62,
+                child: Center(
+                  child: Text(
+                    buttonText,
+                    style: const TextStyle(
+                        color: Color.fromRGBO(55, 80, 255, 1),
+                        fontSize: 25,
+                        fontWeight: FontWeight.w600),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
